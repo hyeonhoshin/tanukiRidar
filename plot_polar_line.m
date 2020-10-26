e834ec6478a896aa8b77c719d2ae080eb5b86ccf
tlsgusghq <tlsgusghq@naver.com>
2020-10-26 11:24:25 +0900

    필요없는 것들 제거.

--- plot_polar_line.m
+++
@@ -1,9 +0,0 @@
-function plot_polar_line(r,a)
-    syms x y
-    fplot(r*sin(a)+(-1/tan(a))*(x-r*cos(a)));
-
-    disp('Slope is'); disp(-1/tan(a));
-    disp('y-offset is'); disp(r*sin(a)+r*cos(a)/tan(a));
-
-    %axis([0 1 0 0.5])
-end
--- rect2polar.m
+++
@@ -1,5 +0,0 @@
-function [r,a] = rect2polar(slope, bias)
-% Convert rectangular line data to line in polar cordinates
-    a = atan2(-1,slope);
-    r = bias.*sin(a);
-end
